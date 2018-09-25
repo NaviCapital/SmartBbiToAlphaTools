fca88a20a413cb748d679909b4c7c5d67d80e62c
Flavio Wuensche <flavio@touts.com.br>
2018-09-25 16:52:42 -0300

    Raise errors if instrument not found or lacking settlement days

--- app/instruments.py
+++ app/instruments.py
@@ -7,8 +7,13 @@ def get_id(symbol, dict = instruments_json):
   for key in dict:
     if dict[key]["symbol"] == symbol:
       return int(dict[key]["id"])
+  raise Exception(f"Instrument '{symbol}' not found")
 
 def get_settlement_days(symbol, dict = instruments_json):
   for key in dict:
     if dict[key]["symbol"] == symbol:
-      return int(dict[key]["settlement_days"])
+      if dict[key]["settlement_days"] is None:
+        raise Exception(f"Instrument '{symbol}' lacks settlement_days")
+      else:
+        return int(dict[key]["settlement_days"])
+  raise Exception(f"Instrument '{symbol}' not found")
